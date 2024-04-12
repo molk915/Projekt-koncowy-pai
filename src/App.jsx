9250c08d35f10808f6728d97c5032b3acca80036
molk915 <qwerkqwerk41@gmail.com>
2024-04-12 11:56:12 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,31 @@ function App() {
   const [quality, setQuality] = useState("1");
   const [itemData, setItemData] = useState(null);
   const [imageUrl, setImageUrl] = useState("");
+  
+  const getItems = async () => {
+    const apiUrl = `http://localhost:5000/${itemname}/${tier}/${enchants}/${quality}/${location}`;
+    try {
+      const response = await axios.get(apiUrl);
+      if (response.data && response.data.length > 0) {
+        const itemInfo =
+          response.data[0].length > 0 ? response.data[0][0] : null;
+        if (itemInfo) {
+          setItemData({
+            sellPriceMax: itemInfo.sell_price_max,
+            sellPriceMin: itemInfo.sell_price_min,
+            buyPriceMax: itemInfo.buy_price_max,
+            buyPriceMin: itemInfo.buy_price_min,
+          });
+        }
+        if (response.data[1]) {
+          setImageUrl(response.data[1]);
+        }
+      }
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+
+  
 
   const getItems = async () => {
     const apiUrl = `http://localhost:5000/${itemname}/${tier}/${enchants}/${quality}/${location}`;
