72e389f1b3e4eb206bdb0838086916627b17ba94
drkysiol <drkysiolek@gmail.com>
2024-05-12 19:40:35 +0200

    s

--- src/components/GoldPriceButton/GoldPriceButton.jsx
+++ src/components/GoldPriceButton/GoldPriceButton.jsx
@@ -1,11 +1,7 @@
 import React, { useState } from "react";
 import "./GoldPriceButton.css";
 
-<<<<<<< HEAD
 const GoldPriceButton = () => {
-=======
-const GoldenPriceButton = () => {
->>>>>>> ac7011014d611340f2cebb95b7e14332b9951bf1
   const [goldPrice, setGoldPrice] = useState(null);
 
   const fetchGoldPrice = async () => {
@@ -23,19 +19,11 @@ const GoldenPriceButton = () => {
   };
 
   return (
-<<<<<<< HEAD
     <div className="GoldPrice">
-=======
-    <div>
->>>>>>> ac7011014d611340f2cebb95b7e14332b9951bf1
       <button onClick={fetchGoldPrice}>Get Gold Price</button>
       {goldPrice && <p>Gold Price: {goldPrice}</p>}
     </div>
   );
 };
 
-<<<<<<< HEAD
 export default GoldPriceButton;
-=======
-export default GoldenPriceButton;
->>>>>>> ac7011014d611340f2cebb95b7e14332b9951bf1
